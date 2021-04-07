7b0bb119f97dee28a5e79b3eefbe7e23474391b1
Chris Zankel <chris@zankel.net>
2021-04-07 07:19:52 -0400

    cli: merge set comands into update
    Move set config to update config. For setting the current workspace,
    the new command is now update project (--workspace|--ws) WORKSPACE
    
    Signed-off-by: Chris Zankel <chris@zankel.net>

--- cli/update.go
+++ cli/update.go
@@ -3,6 +3,7 @@ package cli
 import (
 	"github.com/spf13/cobra"
 
+	"github.com/czankel/cne/config"
 	"github.com/czankel/cne/errdefs"
 	"github.com/czankel/cne/project"
 )
@@ -48,9 +49,89 @@ func updateWorkspaceRunE(cmd *cobra.Command, args []string) error {
 	return err
 }
 
+var updateSystemConfig bool
+
+var updateConfigCmd = &cobra.Command{
+	Use:   "config",
+	Short: "Update the environment configuration",
+	Long: `Update the user or system configuration for the environment.
+By default, the configuration is written to the user configuration file.
+The system option modifies the system-wide configuration file stored in
+/etc, and requires system permissions.`,
+	RunE: updateConfigRunE,
+	Args: cobra.ExactArgs(2),
+}
+
+func updateConfigRunE(cmd *cobra.Command, args []string) error {
+
+	var conf *config.Config
+	if updateSystemConfig {
+		conf = config.LoadSystemConfig()
+	} else {
+		conf = config.LoadUserConfig()
+	}
+
+	name := args[0]
+	newVal := args[1]
+	oldVal, path, err := conf.SetByName(name, newVal)
+	if err != nil {
+		return err
+	}
+
+	if updateSystemConfig {
+		err = conf.WriteSystemConfig()
+	} else {
+		err = conf.WriteUserConfig()
+	}
+
+	if err != nil {
+		return err
+	}
+
+	printList([]struct {
+		Configuration string
+		Value         string
+		Old           string
+	}{{path, newVal, oldVal}})
+	return nil
+}
+
+var updateProjectCmd = &cobra.Command{
+	Use:     "project",
+	Short:   "Update the project",
+	Aliases: []string{"prj"},
+	RunE:    updateProjectRunE,
+	Args:    cobra.NoArgs,
+}
+
+var updateProjectWorkspace string
+
+func updateProjectRunE(cmd *cobra.Command, args []string) error {
+
+	prj, err := project.Load()
+	if err != nil {
+		return err
+	}
+
+	err = prj.SetCurrentWorkspace(updateProjectWorkspace)
+	if err != nil {
+		return err
+	}
+
+	return prj.Write()
+}
+
 func init() {
 	rootCmd.AddCommand(updateCmd)
 	updateCmd.AddCommand(updateWorkspaceCmd)
 	updateWorkspaceCmd.Flags().StringVarP(
 		&updateWorkspaceName, "name", "", "", "Rename the workspace")
+	updateCmd.AddCommand(updateConfigCmd)
+	updateConfigCmd.Flags().BoolVarP(
+		&updateSystemConfig, "system", "", false, "Update system configuration")
+	updateProjectCmd.Flags().StringVar(
+		&updateProjectWorkspace, "workspace", "", "Update current workspace")
+	updateProjectCmd.Flags().StringVar(
+		&updateProjectWorkspace, "ws", "", "Update current workspace")
+	updateCmd.AddCommand(updateProjectCmd)
 }
