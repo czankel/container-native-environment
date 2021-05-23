942f468d9e047ab03ddb033f3558751b2a0826c3
Chris Zankel <chris@zankel.net>
2021-05-23 07:42:38 -0700

    cli: fix console in exec and other improvements
    Executing in a container environment could leave the console in a
    non-default state. The patch creates a new console for exec and resets
    it after completion. Also implement an option to 'execution in shell'.
    
    Signed-off-by: Chris Zankel <chris@zankel.net>

--- cli/exec.go
+++ cli/exec.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/spf13/cobra"
 
+	"github.com/containerd/console"
+
 	"github.com/czankel/cne/container"
 	"github.com/czankel/cne/errdefs"
 	"github.com/czankel/cne/project"
@@ -21,38 +23,47 @@ var execCmd = &cobra.Command{
 
 var execShell bool
 
-func execRunE(cmd *cobra.Command, args []string) error {
+// execCommandsInShell executes the provided commands in a shell.
+func execCommandsInShell(wsName, layerName string, args []string) (int, error) {
+	args = append([]string{"/bin/sh", "-c"}, args...)
+	return execCommands(wsName, layerName, args)
+}
 
-	prj, err := project.Load()
-	if err != nil {
-		return err
-	}
-
-	ws, err := prj.CurrentWorkspace()
-	if err != nil {
-		return err
-	}
+// execCommands executes the provided commands in the current or provided workspace.
+// It returns a code != 0 if the executed command failed. The returned 'code' value
+// is the value returned by the command. The caller should call exit(code) to have a
+// similar return value as if the command was executed directly.
+func execCommands(wsName, layerName string, args []string) (int, error) {
 
 	run, err := runtime.Open(conf.Runtime)
 	if err != nil {
-		return err
+		return 0, err
 	}
 	defer run.Close()
 
-	ctr, err := container.Get(run, ws)
-	if err != nil && !errors.Is(err, errdefs.ErrNotFound) {
-		return err
+	prj, err := project.Load()
+	if err != nil {
+		return 0, err
 	}
 
-	if ctr == nil {
-		ctr, err = buildContainer(run, ws)
+	var ws *project.Workspace
+	if wsName == "" {
+		ws, err = prj.CurrentWorkspace()
 		if err != nil {
-			return err
+			return 0, err
 		}
 	}
 
-	if execShell {
-		args = append([]string{"/bin/sh", "-c"}, args...)
+	ctr, err := container.Get(run, ws)
+	if err != nil && !errors.Is(err, errdefs.ErrNotFound) {
+		return 0, err
+	}
+	if ctr == nil {
+		ctr, err = buildContainer(run, ws)
+		if err != nil {
+			return 0, err
+		}
+		prj.Write()
 	}
 
 	stream := runtime.Stream{
@@ -61,16 +72,41 @@ func execRunE(cmd *cobra.Command, args []string) error {
 		Stderr:   os.Stderr,
 		Terminal: true,
 	}
+	con := console.Current()
+	defer con.Reset()
+
+	// TODO: check return errors?
+	con.SetRaw()
+	winSz, _ := con.Size()
+	con.Resize(winSz)
 
 	code, err := ctr.Exec(&user, stream, args)
+	if err != nil && errors.Is(err, errdefs.ErrNotFound) {
+		return 0, errors.New(args[0] + ": no such command")
+	}
 	if err != nil {
-		return err
+		return int(code), nil
 	}
 
-	os.Exit(int(code))
-
-	return nil
+	return 0, nil
 }
+
+func execRunE(cmd *cobra.Command, args []string) error {
+
+	var code int
+	var err error
+
+	if execShell {
+		code, err = execCommandsInShell("", "", args)
+	} else {
+		code, err = execCommands("", "", args)
+	}
+	if code != 0 {
+		os.Exit(code)
+	}
+	return err
+}
+
 func init() {
 	execCmd.Flags().BoolVarP(&execShell, "", "c", false,
 		"Start a shell for the provided commands")
