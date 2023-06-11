560ba1e18b255a128f2a158148141a807b2108f7
Chris Zankel <chris@zankel.net>
2023-06-11 11:41:07 -0700

    cli: implement managing the current context
    Add context command to show the current context or to change it. When
    setting the context, it can be on a user or project basis.
    
    Signed-off-by: Chris Zankel <chris@zankel.net>

--- cli/context.go
+++ cli/context.go
@@ -1,31 +1,78 @@
 package cli
 
 import (
-	"fmt"
+	"errors"
 
+	"github.com/czankel/cne/config"
+	"github.com/czankel/cne/errdefs"
 	"github.com/spf13/cobra"
 )
 
 var contextCmd = &cobra.Command{
 	Use:   "context",
-	Short: "Show or get the current context",
-	Long: `The context allows users to swich quickly between different
+	Short: "Show or set the current context",
+	Long: `... can be overwritten using --project or --user
 configurations, mainly the runtime but also other settings.`,
 	RunE: contextRunE,
 	Args: cobra.RangeArgs(0, 1),
 }
 
+var configUser bool
+
 func contextRunE(cmd *cobra.Command, args []string) error {
 
-	if len(args) == 0 {
-		fmt.Printf("Context: %s\n", conf.Settings.Context)
-	} else {
-		// FIXME: check if context exists or error; set context an save config
+	var name string
+	var err error
+	var ctx *config.Context
 
+	if len(args) == 0 {
+		ctx, name, err = conf.GetContext()
+	} else {
+
+		var tempConf *config.Config
+		var found bool
+
+		name = args[0]
+
+		if ctx, found = conf.Context[name]; !found {
+			return errdefs.NotFound("context", name)
+		}
+
+		_, err := loadProject()
+		if configProject && err != nil {
+			return err
+		}
+		if !configUser && !configProject && err == nil {
+			configProject = true
+			tempConf, err = loadConfig()
+			configUser = err != nil
+			configProject = err == nil
+		}
+		if configUser || (configProject && err == nil) {
+			tempConf, err = loadConfig()
+		}
+		if err != nil && !errors.Is(err, errdefs.ErrNotFound) {
+			return err
+		} else if err != nil {
+			tempConf = &config.Config{Settings: config.Settings{}}
+		}
+
+		tempConf.Settings.Context = name
+		err = writeConfig(tempConf)
 	}
+
+	if err != nil {
+		return err
+	}
+
+	printList(map[string]*config.Context{name: ctx}, false)
 	return nil
 }
 
 func init() {
 	rootCmd.AddCommand(contextCmd)
+	contextCmd.Flags().BoolVarP(
+		&configUser, "system", "", false, "Set context for user")
+	contextCmd.Flags().BoolVarP(
+		&configProject, "project", "", false, "Set context for project")
 }
